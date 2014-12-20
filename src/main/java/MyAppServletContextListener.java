29a2f3ec31ddd4307a965ade6532ffee68c2b968
Krishna Pujara <kpujara@equalexperts.com>
2014-12-20 09:36:06 +0000

    Intial commit to print Hello Quartz! every minute. Added MyAppServletContextListener which will trigger HelloJob when servlet context beign initialized. Also added quartz basic configuration in the properties file.

---
+++ src/main/java/MyAppServletContextListener.java
@@ -0,0 +1,40 @@
+import org.quartz.*;
+import org.quartz.ee.servlet.QuartzInitializerListener;
+import org.quartz.impl.StdSchedulerFactory;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class MyAppServletContextListener implements ServletContextListener{
+
+    @Override
+    public void contextDestroyed(ServletContextEvent arg0) {
+        System.out.println("ServletContextListener destroyed");
+    }
+
+    //Run this before web application is started
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        System.out.println("ServletContextListener started");
+        ServletContext ctx = servletContextEvent.getServletContext();
+        StdSchedulerFactory factory = (StdSchedulerFactory) ctx.getAttribute(QuartzInitializerListener.QUARTZ_FACTORY_KEY);
+
+        JobDetail job = JobBuilder.newJob(HelloJob.class).withIdentity("dummyJobName", "group1").build();
+
+        Trigger trigger = TriggerBuilder.newTrigger()
+                .withIdentity("dummyTriggerName", "group1")
+                .withSchedule(CronScheduleBuilder.cronSchedule("0 0/1 * 1/1 * ? *"))
+                .build();
+
+        //schedule it
+        Scheduler scheduler = null;
+        try{
+            scheduler = factory.getScheduler();
+            scheduler.start();
+            scheduler.scheduleJob(job, trigger);
+        }catch(SchedulerException schedulerException){
+            schedulerException.printStackTrace();
+        }
+    }
+}
